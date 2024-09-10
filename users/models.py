fe78b6bb1c04f8444b714e38bd9c3ffc9d03e5f5
Firdavs222 <119061460+Firdavs222@users.noreply.github.com>
2024-09-10 11:47:16 +0500

    <Updated>

--- users/models.py
+++ users/models.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import AbstractUser
 from django_resized import ResizedImageField
 import os
 import uuid
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.core import validators
+from users.errors import BIRTH_YEAR_ERROR_MSG
 # Create your models here.
 
 def file_upload(instance, filename):
@@ -15,12 +19,36 @@ class CustomUser(AbstractUser):
     middle_name = models.CharField(max_length=30, blank=True, null=True)
     avatar = models.ImageField(upload_to=file_upload, blank=True)
     avatar = ResizedImageField(size=[300, 300], crop=['top', 'left'], upload_to=file_upload, blank=True)
+    birth_year = models.IntegerField(
+		        validators=[  # tug'ilgan yil oralig'ini tekshirish uchun birinchi variant
+		            validators.MinValueValidator(settings.BIRTH_YEAR_MIN),
+		            validators.MaxValueValidator(settings.BIRTH_YEAR_MAX)
+		        ],
+		        null=True,
+		        blank=True
+		    )
+    def clean(self):
+        super().clean()
+        if self.birth_year and not (settings.BIRTH_YEAR_MIN < self.birth_year < settings.BIRTH_YEAR_MAX):
+            raise ValidationError(BIRTH_YEAR_ERROR_MSG)
+        
+        def save(self, *args, **kwargs):
+            self.clean()
+            super().save(*args, **kwargs)
     
     class Meta:
         db_table = "user"
         verbose_name = "User"
         verbose_name_plural = "Users"
         ordering = ["-date_joined"]
+        constraints = [
+                    models.CheckConstraint(  # tug'ilgan yil oralig'ini tekshirish uchun uchunchi variant
+                        check=models.Q(birth_year__gt=settings.BIRTH_YEAR_MIN) & models.Q(
+                            birth_year__lt=settings.BIRTH_YEAR_MAX),
+                        name='check_birth_year_range'
+                    )
+                ]
+        
     def __str__(self):
         if self.full_name:
             return self.full_name
